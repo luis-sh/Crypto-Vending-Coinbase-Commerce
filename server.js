30fa7d3d993793afbc7f9b6578443d6aaaaa947c
Luis Dominguez <ld@luis.sh>
2019-09-07 21:49:46 +0000

    Writing out sample coinbase-endpoint code

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const CoinbaseCommerce = require("./integrations/coinbase-commerce");
 
 let app = express();
 
-app.get("/coinbase-endpoint", async (req, res) => {
+app.get("/coinbase-endpoint", async (req, res, next) => {
+  if(!req.query) { next(); }
+
+  const ev = req.query.event;
+  if(ev.type === "charge:pending") {
+    console.log(`Charge pending! Data: ${JSON.parse(JSON.stringify(req.query))}`);
+  
+  } else if (ev.type === "charge:failed") {
+    //To some extent, we are failing silently
+    console.log(`Charge Failed! Data: ${JSON.parse(JSON.stringify(req.query))}`);
+  }
+});
+
+app.get("/coinbase-endpoint-test", async (req, res) => {
   let coinbaseCommerce = new CoinbaseCommerce(process.env.COINBASE_COMMERCE_API_KEY);
   coinbaseCommerce.runTest();
 });
