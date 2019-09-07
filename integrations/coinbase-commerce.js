9b363e8e66ca59862cdd33996ad0de2e41731199
Luis Dominguez <ld@luis.sh>
2019-09-07 21:28:09 +0000

    Add runTest() for shooting off charges (just for a minor test), and continue building out structure

--- integrations/coinbase-commerce.js
+++ integrations/coinbase-commerce.js
@@ -13,6 +13,37 @@ class CoinbaseCommerce {
     this.client = Client.init(API_KEY);
   }
 
+  handleEndpoint() {
+  
+  
+  }
+
+  async runTest() {
+
+    let someCharge = new Charge({
+      "name": "First Charge For Fun",
+      "description": "Mastering how to destroy all memes",
+      "logo_url": "https://scontent-sjc3-1.cdninstagram.com/vp/d749b74ff0e00818cc4e9bdcb4406e1b/5DFE2A58/t51.2885-15/e35/65997172_146976953043568_7513483871545499946_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com",
+      "metadata": {
+        "customer_name": "anonymous hooligan"
+      },
+      "payments": [],
+      "pricing_type": "no_price",
+      "addresses": {
+        "bitcoin": "mymZkiXhQNd6VWWG7VGSVdDX9bKmviti3U",
+        "ethereum": "0x0000000000000000000000000000000000000000"
+      }
+    });
+
+    console.log("Starting to charge");
+    const res = await someCharge.save();
+    console.log("Seems we've created it?");
+    console.log(JSON.parse(JSON.stringify(res)));
+
+    console.log(`Response ID: ${res.id}`);
+  
+  }
+
 }
 
 
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 
 const express = require("express");
+const CoinbaseCommerce = require("./integrations/coinbase-commerce");
 
 let app = express();
 
-app.get("/coinbase-endpoint", (req, res) => {
-  
+app.get("/coinbase-endpoint", async (req, res) => {
+  let coinbaseCommerce = new CoinbaseCommerce(process.env.COINBASE_COMMERCE_API_KEY);
+  coinbaseCommerce.runTest();
 });
 
-app.use(function(req, res){
-   res.status(404);
-   res.end("404: File Not Found");
+app.use((req, res) => {
+  res.status(404);
+  res.end("404: File Not Found");
 });
 
 module.exports = app;
